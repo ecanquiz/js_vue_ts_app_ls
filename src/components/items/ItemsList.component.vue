6474c1b16799af167a70f6606d74edfb5b3de8e8
ecanquiz <fdsoil123@gmail.com>
2021-04-17 19:27:20 -0400

    feat: Chapter 3 - Data Model Interfaces

--- src/components/items/ItemsList.component.vue
+++ src/components/items/ItemsList.component.vue
@@ -2,17 +2,20 @@
   <div>
     <h3>Items:</h3>
     <ul>
-      <li v-for="item in items" :key="item.id">{{ item.name }}</li>
+      <li v-for="item in items" :key="item.id">
+        {{ item.name }}
+      </li>
     </ul>
   </div>
 </template>
 <script lang="ts">
   import { defineComponent, PropType } from 'vue'
+  import { ItemInterface } from '@/models/items/Item.interface'
 
   export default defineComponent({
     props: {
       items: {
-        type: Array as PropType<any[]>
+        type: Array as PropType<ItemInterface[]>
       }
     }
   })
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,6 +6,7 @@
 <script lang="ts">
   import { defineComponent } from 'vue'
   import ItemsListComponent from '@/components/items/ItemsList.component.vue'
+  import { ItemInterface } from '@/models/items/Item.interface'
 
   export default defineComponent({
     name:'Home',
@@ -17,13 +18,16 @@
       const items: any[] = [
         {
           id: 1,
-          name: 'Item 1'
+          name: 'Item 1',
+          selected: false
         }, {
           id: 2,
-          name: 'Item 2'
+          name: 'Item 2',
+          selected: false
         }, {
           id: 3,
-          name: 'Item 3'
+          name: 'Item 3',
+          selected: false
         }
       ]
       
