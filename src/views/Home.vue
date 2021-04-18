4a83d89e2ebbc411686a8b65be6d665aa9490e02
ecanquiz <fdsoil123@gmail.com>
2021-04-17 22:08:58 -0400

    feat: Chapter 6 - Introducing Vuex

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,14 @@
 <template>
   <div class="home">
-    <ItemsListComponent :items="items"/>
+    <ItemsListComponent
+      :items="items"
+      :loading="loading"
+      @selectItem="onSelectItem" />
   </div>
 </template>
 <script lang="ts">
-  import { defineComponent, reactive } from 'vue'
+  import { defineComponent, reactive, computed, onMounted } from 'vue'
+  import store from'@/store'
   import ItemsListComponent from '@/components/items/ItemsList.component.vue'
   import { ItemInterface } from '@/models/items/Item.interface'
 
@@ -15,24 +19,29 @@
     },
     setup() {
     
-      const items: ItemInterface[] = reactive([
-        {
-          id: 1,
-          name: 'Item 1',
-          selected: false
-        }, {
-          id: 2,
-          name: 'Item 2',
-          selected: false
-        }, {
-          id: 3,
-          name: 'Item 3',
-          selected: false
-        }
-      ])
+      const items = computed(() => {
+        return store.state.items
+      })
+      
+      const loading = computed(() => {
+        return store.state.loading
+      })
+      
+      onMounted(() => {
+        store.dispatch('loadItems')
+      })
+
+      const onSelectItem = (item: ItemInterface) => {
+        store.dispatch('selectItem', {
+          id: item.id,
+          selected:!item.selected
+        })
+      }
       
       return {
-        items
+        items,
+        loading,
+        onSelectItem
       }
       
     }
